c2c73cc10ea92cc10266c2058ab159d6f152ad0a
Marian1224 <marjan1715@gmail.com>
2019-11-29 22:43:48 +0200

    add build folder

---
+++ build/precache-manifest.c7d7f810675464305b1c35708b56d062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fd73940df9a865dce096206f7a2d1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55f3fe69dc6818b21ca0",
+    "url": "/static/css/main.d75a0d91.chunk.css"
+  },
+  {
+    "revision": "8e259694280e6dfc8b9d",
+    "url": "/static/js/2.c314c5b6.chunk.js"
+  },
+  {
+    "revision": "55f3fe69dc6818b21ca0",
+    "url": "/static/js/main.ab18e7e1.chunk.js"
+  },
+  {
+    "revision": "ee5810dd8882a658d71e",
+    "url": "/static/js/runtime~main.d67e043c.js"
+  }
+]);
