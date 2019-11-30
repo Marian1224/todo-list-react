46d6dc87aa36d636ca82d64892242ec022653fc9
Marian1224 <marjan1715@gmail.com>
2019-11-30 03:08:52 +0200

    Updates

---
+++ precache-manifest.a420b86c0a3728256e442e0607cb26d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a8b8e7e06691ba66db4ff60b11beb3",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "55f3fe69dc6818b21ca0",
+    "url": "/reactTodoList/static/css/main.d75a0d91.chunk.css"
+  },
+  {
+    "revision": "8e259694280e6dfc8b9d",
+    "url": "/reactTodoList/static/js/2.c314c5b6.chunk.js"
+  },
+  {
+    "revision": "55f3fe69dc6818b21ca0",
+    "url": "/reactTodoList/static/js/main.ab18e7e1.chunk.js"
+  },
+  {
+    "revision": "09eb8d214cbe5b600fab",
+    "url": "/reactTodoList/static/js/runtime~main.dee56807.js"
+  }
+]);
